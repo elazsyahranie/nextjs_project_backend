a9216ca877001c8642ca306a3692e2cd73e0a1a7
bagus tri harjanto <bagustri15@gmail.com>
2021-05-25 16:24:33 +0700

    Add socket io

--- src/index.js
+++ src/index.js
@@ -7,6 +7,9 @@ const helmet = require('helmet')
 const compression = require('compression')
 const bodyParser = require('body-parser')
 const routerNavigation = require('./routes')
+// =========================================
+const socket = require('socket.io')
+// =========================================
 
 const app = express()
 const port = process.env.DB_PORT
@@ -24,6 +27,49 @@ app.use(bodyParser.json())
 app.use('/backend3/api/v1', routerNavigation)
 app.use('/backend3/api', express.static('src/uploads'))
 
-app.listen(port, () => {
+// =========================================
+const server = require('http').createServer(app)
+const io = socket(server, {
+  cors: {
+    origin: '*'
+  },
+  path: '/backend3/socket.io'
+})
+io.on('connection', (socket) => {
+  console.log('Socket.io Connect !')
+  // globalMessage = pesan yang dikirikan ke semua client
+  socket.on('globalMessage', (data) => {
+    console.log(data)
+    io.emit('chatMessage', data)
+  })
+  // privateMessage = pesan yang dikirikan ke client saja
+  socket.on('privateMessage', (data) => {
+    console.log(data)
+    socket.emit('chatMessage', data)
+  })
+  // broadcastMessage = pesan yang dikirikan ke semua client kecuali si pengirim
+  socket.on('broadcastMessage', (data) => {
+    console.log(data)
+    socket.broadcast.emit('chatMessage', data)
+  })
+  // =========================================
+  socket.on('joinRoom', (data) => {
+    console.log(data)
+    if (data.oldRoom) {
+      socket.leave(data.oldRoom)
+    }
+    socket.join(data.room)
+    socket.broadcast.to(data.room).emit('chatMessage', {
+      username: 'BOT',
+      message: `${data.username} Joined Chat !`
+    })
+  })
+  socket.on('roomMessage', (data) => {
+    io.to(data.room).emit('chatMessage', data)
+  })
+})
+// =========================================
+
+server.listen(port, () => {
   console.log(`Express app is listen on port ${port} !`)
 })
