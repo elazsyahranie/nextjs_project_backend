1af8c04aa91a6e371b3e894525bc152b5a1b5f7e
elazsyahranie <elazaribrahims@gmail.com>
2021-05-28 16:37:02 +0700

    Fix resigtration

--- src/config/mysql.js
+++ src/config/mysql.js
@@ -5,7 +5,7 @@ const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database: process.env.DB_NAME
+  database: process.env.DB_DATABASES
 })
 
 connection.connect((error) => {
--- src/modules/auth/auth_model.js
+++ src/modules/auth/auth_model.js
@@ -1,9 +1,10 @@
 const connection = require('../../config/mysql')
 
 module.exports = {
-  register: (ydata) => {
+  register: (data) => {
     return new Promise((resolve, reject) => {
       connection.query('INSERT INTO user SET ?', data, (error, result) => {
+        console.log(error)
         if (!error) {
           const newResult = {
             id: result.insertId,
